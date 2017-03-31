2988f20da10fe39bd5caeb18740e377b253483d1
patrick vares <pvrs12@gmail.com>
2017-03-31 03:55:37 +0000

    removed old makefiles, added midi library and header files, refactored ifs.cpp

--- music/cppifs/src/ifs.cpp
+++ music/cppifs/src/ifs.cpp
@@ -5,6 +5,18 @@
 #include <fstream>
 #include <random>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <midi.h>
+#include <midi_helper.h>
+#include <midi_constants.h>
+
+#ifdef __cplusplus
+}
+#endif
+
 int RES = 10000;
 
 int WIDTH = 1000;
@@ -43,6 +55,9 @@ struct equ {
 	}
 };
 
+typedef std::vector<std::pair<color, std::pair<double, double>>> point_field;
+typedef std::vector<std::vector<color>> image_field;
+
 std::vector<color> COLOR_LIST = {
 	color(255,0,0),
 	color(255,255,0),
@@ -56,7 +71,7 @@ double scale(double x, double in_min, double in_max, double out_min, double out_
 	return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
-void write_pbm(const std::vector<std::vector<color>>& img, std::ostream& os){
+void write_pbm(const image_field& img, std::ostream& os){
 	os << "P3\n";
 	os << img.size() << " " << img[0].size() << "\n";
 	os << "255\n";
@@ -68,35 +83,9 @@ void write_pbm(const std::vector<std::vector<color>>& img, std::ostream& os){
 	}
 }
 
-int main(int argc, char* argv[]){
+point_field fill_points(const std::vector<equ>& equs, const std::vector<double>& weights, double& min_x, double& max_x, double& min_y, double& max_y){
+	point_field points;
 
-	if(argc > 1){
-		RES = atoi(argv[1]);
-		if(argc > 3){
-			WIDTH = atoi(argv[2]);
-			HEIGHT = atoi(argv[3]);
-			if(argc > 4){
-				USE_COLOR = true;
-			}
-		}
-	}
-
-	std::vector<equ> equs;
-	std::vector<double> weights;
-
-	std::vector<std::pair<color, std::pair<double, double>>> points;
-
-	while(true){
-		equ e;
-		double p;
-		std::cin >> e.a >> e.b >> e.c >> e.d >> e.e >> e.f >> p;
-		if( !e.a && !e.b && !e.c && !e.d && !e.e && !e.f && !p){
-			break;
-		}
-		e.col = COLOR_LIST[equs.size() % COLOR_LIST.size()];
-		equs.push_back(e);
-		weights.push_back(p);
-	}
 	//generate random numbers with weights
 	std::discrete_distribution<> dist(weights.begin(), weights.end());
 	std::random_device rd;
@@ -104,7 +93,6 @@ int main(int argc, char* argv[]){
 
 	//initial X and Y
 	double x = 1, y = 1;
-	double min_x= 100, min_y = 100, max_x = -100, max_y = -100;
 	for(int i = 0;i < RES; ++i){
 		int n = dist(gen);
 		equ e = equs[n];
@@ -127,13 +115,16 @@ int main(int argc, char* argv[]){
 		y = e.apply_y(x,y);
 		x = tx;
 	}
-	std::vector<std::vector<color>> image(WIDTH + 1, std::vector<color>(HEIGHT + 1, color(255, 255, 255)));
+	return points;
+}
 
-	for(std::pair<color,std::pair<double, double>>& pp: points){
+image_field get_image(const point_field& points, double min_x, double max_x, double min_y, double max_y){
+	image_field image(WIDTH + 1, std::vector<color>(HEIGHT + 1, color(255, 255, 255)));
+
+	for(const std::pair<color,std::pair<double, double>>& pp: points){
 		std::pair<double,double> p = pp.second;
 		int image_x = scale(p.first - min_x, 0, max_x - min_x, 0, WIDTH);
 		int image_y = scale(p.second - min_y, 0, max_y - min_y, 0, HEIGHT);
-		//std::cout<<image_x<<" "<<image_y<<std::endl;
 
 		if(USE_COLOR){
 			image[image_x][image_y] = pp.first;
@@ -141,5 +132,40 @@ int main(int argc, char* argv[]){
 			image[image_x][image_y] = color(0,0,0);
 		}
 	}
+	return image;
+}
+
+int main(int argc, char* argv[]){
+
+	if(argc > 1){
+		RES = atoi(argv[1]);
+		if(argc > 3){
+			WIDTH = atoi(argv[2]);
+			HEIGHT = atoi(argv[3]);
+			if(argc > 4){
+				USE_COLOR = true;
+			}
+		}
+	}
+
+	std::vector<equ> equs;
+	std::vector<double> weights;
+
+	while(true){
+		equ e;
+		double p;
+		std::cin >> e.a >> e.b >> e.c >> e.d >> e.e >> e.f >> p;
+		if( !e.a && !e.b && !e.c && !e.d && !e.e && !e.f && !p){
+			break;
+		}
+		e.col = COLOR_LIST[equs.size() % COLOR_LIST.size()];
+		equs.push_back(e);
+		weights.push_back(p);
+	}
+
+	double min_x= 100, min_y = 100, max_x = -100, max_y = -100;
+	point_field points = fill_points(equs, weights, min_x, max_x, min_y, max_y);
+	image_field image = get_image(points, min_x, max_x, min_y, max_y);
+
 	write_pbm(image, std::cout);
 }
--- music/cppifs/src/include/test2.h
+++
@@ -1,6 +0,0 @@
-#ifndef TEST2_H
-#define TEST2_H 
-
-void func();
-
-#endif /* TEST2_H */
