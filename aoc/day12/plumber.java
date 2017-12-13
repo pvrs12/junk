5cf08020e25e3a8a17009e6e00be3e2a1e317215
pvrs12 <pvrs12@gmail.com>
2017-12-12 23:23:38 -0500

    That was easy...

---
+++ aoc/day12/plumber.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class plumber {
+
+    static HashSet<Integer> visited = new HashSet<>();
+
+    public static int get_connected(HashMap<Integer, ArrayList<Integer>> dataset, int start){
+        if(visited.contains(start)){
+            return 0;
+        }
+        visited.add(start);
+
+        int total = 1;
+        for(int i : dataset.get(start)){
+            total += get_connected(dataset, i);
+        }
+        return total;
+    }
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+
+        HashMap<Integer, ArrayList<Integer>> dataset = new HashMap<>();
+
+        while(in.hasNextLine()){
+            String line = in.nextLine();
+            if(line.length() < 1){
+                continue;
+            }
+            
+            String[] parts = line.split("<->");
+            int n = Integer.parseInt(parts[0].trim());
+            String[] connections = parts[1].split(",");
+
+            ArrayList<Integer> conns = new ArrayList<>();
+            for(String s :connections){
+                conns.add(Integer.parseInt(s.trim()));
+            }
+            dataset.put(n, conns);
+        }
+
+        //start at 0, DFS and count unique entries
+        System.out.println(get_connected(dataset, 0));
+        in.close();
+    }
+}
