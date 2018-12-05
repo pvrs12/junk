b74e07841d901559206e2435276815069f0d93e9
pvrs12 <pvrs12@gmail.com>
2018-12-05 10:44:41 -0500

    day5 complete. That was so janky

--- aoc/2018/day01/main.c
+++ aoc/2018/day01/main.c
@@ -3,9 +3,11 @@
 
 int main() {
     int num, val=0, val_len=0, num_len=0;
-    long long val_capacity=10L, num_capacity=10L;
+    /*long long val_capacity=10L, num_capacity=10L;*/
+    int val_capacity = 10, num_capacity=10;
     int *vals = malloc(sizeof(int) * val_capacity);
     int *nums = malloc(sizeof(int) * num_capacity);
+    int no_part2=0;
     while(scanf("%d", &num) != EOF) {
         nums[num_len++] = num;
         if (num_len >= num_capacity) {
@@ -16,6 +18,7 @@ int main() {
         for (int i=0; i<val_len; ++i){
             if (vals[i] == val) {
                 printf("part 2 answer=%d\n", val);
+                no_part2=1;
             }
         }
         /*printf("%d\n", val);*/
@@ -26,26 +29,35 @@ int main() {
         }
     }
     printf("part 1 answer=%d\n", val);
-    while(1) {
-        for(int i=0; i<num_len; ++i) {
-            val += nums[i];
-            for( int j=0; j<val_len; ++j){
-                if (vals[j] == val) {
-                    printf("part 2 answer=%d\n", val);
-                    return 0;
+    int max_val = 0;
+    if (!no_part2) {
+        while(1) {
+            for(int i=0; i<num_len; ++i) {
+                val += nums[i];
+                if (val > max_val) {
+                    max_val = val;
                 }
-            }
-            vals[val_len++] = val;
+                if (-val > max_val) {
+                    max_val = -val;
+                }
+                for( int j=0; j<val_len; ++j){
+                    if (vals[j] == val) {
+                        printf("part 2 answer=%d\n", val);
+                        printf("max_val = %d\n", max_val);
+                        free(vals);
+                        free(nums);
+                        return 0;
+                    }
+                }
+                vals[val_len++] = val;
 
-            if (val_len >= val_capacity) {
-                val_capacity *= 2;
-                val_capacity *= 2;
-                vals = realloc(vals, sizeof(int) * val_capacity);
+                if (val_len >= val_capacity) {
+                    val_capacity *= 2;
+                    vals = realloc(vals, sizeof(int) * val_capacity);
+                }
             }
         }
     }
-    free(vals);
-    free(nums);
 
     return 0;
 }
---
+++ aoc/2018/day05/main.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    /*char s[50005];*/
+    /*scanf("%50005s", s);*/
+    /*printf("%s\n", s);*/
+
+    for (char a='a'; a<='z'; ++a){
+        int i=1;
+        while(1) {
+            char in_name[40], out_name[40];
+            sprintf(in_name, "inputs/%c/input_%03d", a, i);
+            sprintf(out_name, "inputs/%c/input_%03d", a, ++i);
+            /*printf("%s\t%s\n", in_name, out_name);*/
+            FILE* inf = fopen(in_name, "r");
+            FILE* outf = fopen(out_name, "w");
+
+            while(1) {
+                char c = fgetc(inf);
+                if (c==EOF || c == '\n') {
+                    break;
+                }
+                if (tolower(c)==a){
+                    continue;
+                }
+                char d = fgetc(inf);
+                if (d==EOF || d == '\n') {
+                    ungetc(d, inf);
+                    /*continue;*/
+                }
+                if ((d==toupper(c) && !isupper(c))
+                    || (d==tolower(c) && !islower(c))) {
+                    //bond
+                    /*printf("removing %c%c.\n", c, d);*/
+                    /*fprintf(stderr, "removing %c%c\n", c, d);*/
+                } else {
+                    /*fputc(c, stdout);*/
+                    fputc(c, outf);
+                    ungetc(d, inf);
+                }
+            }
+            long ins = ftell(inf);
+            long outs = ftell(outf);
+            /*printf("\n");*/
+            /*printf("%ld\t%ld\n", ins, outs);*/
+            fclose(inf);
+            fclose(outf);
+
+            if(ins == outs) {
+                printf("%ld\t%s\n", outs, out_name);
+                break;
+            }
+        }
+    }
+
+    return 0;
+}
