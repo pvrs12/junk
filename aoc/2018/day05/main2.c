62a9f10c9a13a2f4221ce1d417568ba9f326bf34
pvrs12 <pvrs12@gmail.com>
2018-12-05 11:39:23 -0500

    fixed any compiler warnings for -Weverything

--- aoc/2018/day05/main2.c
+++ aoc/2018/day05/main2.c
@@ -3,31 +3,32 @@
 #include <stdlib.h>
 
 int main() {
-    for(char a='a'; a<='z'+1; ++a){
-        int stack_len=0, stack_cap=100;
-        char* stack = malloc(sizeof(char) * stack_cap);
-        while(1){
-            char c = fgetc(stdin);
-            if(c == EOF){
-                break;
-            }
-            char d = stack[stack_len-1];
-            if((toupper(d) == c && !isupper(d)) ||
-                (tolower(d) == c && !islower(d))) {
-                //pop d
-                stack_len--;
-            } else {
-                if (tolower(c) != a) {
-                    stack[stack_len++] = c;
-                    if (stack_len >= stack_cap) {
-                        stack_cap *= 2;
-                        stack = realloc(stack, sizeof(char) * stack_cap);
-                    }
-                }
-            }
+  size_t stack_cap=100;
+  char* stack = malloc(sizeof(char) * stack_cap);
+  for(char a='a'; a<='z'+1; ++a){
+    size_t stack_len=0;
+    while(1){
+      char c = (char)fgetc(stdin);
+      if(c == EOF){
+        break;
+      }
+      char d = stack[stack_len-1];
+      if((toupper(d) == c && !isupper(d)) ||
+        (tolower(d) == c && !islower(d))) {
+        //pop d
+        stack_len--;
+      } else {
+        if (tolower(c) != a) {
+          stack[stack_len++] = c;
+          if (stack_len >= stack_cap) {
+            stack_cap *= 2;
+            stack = realloc(stack, sizeof(char) * stack_cap);
+          }
         }
-        fseek(stdin, 0, 0);
-        printf("%c\t%d\n", a, stack_len -1);
-        free(stack);
+      }
     }
+    fseek(stdin, 0, 0);
+    printf("%c\t%lu\n", a, stack_len -1);
+  }
+  free(stack);
 }
